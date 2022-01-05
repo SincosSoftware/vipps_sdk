58bc9e47a90c1f3f547f471a19123d771ec7d2ff
Roger Lysberg <roger@24nettbutikk.no>
2022-01-05 11:03:09 +0100

    Constructor and first getter

--- src/SystemInfo.php
+++ src/SystemInfo.php
@@ -4,4 +4,15 @@ namespace SincosSoftware\Vipps;
 
 class SystemInfo implements SystemInfoInterface
 {
+    private $systemName;
+
+    public function __construct($systemName)
+    {
+        $this->systemName = $systemName;
+    }
+
+    public function getSystemName()
+    {
+        return $this->systemName;
+    }
 }
