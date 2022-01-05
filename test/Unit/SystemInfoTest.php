673978703e7bed4fdb899d3d54b426995a3c52b2
Roger Lysberg <roger@24nettbutikk.no>
2022-01-05 10:59:26 +0100

    Create systemInfo and check that we can get name

--- test/Unit/SystemInfoTest.php
+++ test/Unit/SystemInfoTest.php
@@ -3,8 +3,17 @@
 namespace SincosSoftware\Vipps\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use SincosSoftware\Vipps\SystemInfo;
 
 class SystemInfoTest extends TestCase
 {
+    /**
+     * @covers \SincosSoftware\Vipps\SystemInfo::__construct()
+     */
+    public function testConstruct()
+    {
+        $systemInfo = new SystemInfo('Vipps-System-Name');
 
+        $this->assertEquals('Vipps-System-Name', $systemInfo->getName());
+    }
 }
